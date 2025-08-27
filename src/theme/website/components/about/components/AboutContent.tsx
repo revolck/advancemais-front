ce7bc89ae1407a55df7a3b0312dbb50ce4557da4
Filipe Marques <filipe@revolck.com.br>
2025-08-27 17:04:27 -0300

    atualizando informações do component

--- src/theme/website/components/about/components/AboutContent.tsx
+++ src/theme/website/components/about/components/AboutContent.tsx
@@ -5,12 +5,10 @@ import { AboutContentProps } from "@/api/websites/components";
 const AboutContent = ({ title, description }: AboutContentProps) => {
   return (
     <div className="w-full lg:w-1/2 lg:text-left">
-      <h1 className="text-3xl mb-4 text-neutral-600">
-        <span className="font-bold">{title}</span>
+      <h1 className="text-[var(--primary-color)] font-bold !leading-none">
+        {title}
       </h1>
-      <p className="text-neutral-400 leading-relaxed text-justify lg:text-left text-base lg:text-lg">
-        {description}
-      </p>
+      <p className="!leading-relaxed !text-justify">{description}</p>
     </div>
   );
 };
